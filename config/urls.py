481acce01b94f639785613568b34d58a2c123dea
HeeeeeJinJeong <misakiyoshikuni@gmail.com>
2019-10-10 17:51:13 +0900

    static 파일 적용

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
     path('', include('reservation.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- reservation/forms.py
+++ reservation/forms.py
@@ -11,11 +11,15 @@ class ReservationForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         self.fields['p_name'].label = "입금자 명"
+        self.fields['p_name'].widget.attrs = {'class': "form-control", 'placeholder': "입금자 명을 입력해주세요"}
 
         self.fields['start_date'].label = "시작시간"
         self.fields['start_date'].widget = forms.DateInput(format='2019-10-10 10:00')
+        self.fields['start_date'].widget.attrs = {'class': "form-control", 'placeholder': ""}
 
         self.fields['end_date'].label = "종료시간"
         self.fields['end_date'].widget = forms.DateInput(format='2019-10-10 12:00')
+        self.fields['end_date'].widget.attrs = {'class': "form-control", 'placeholder': ""}
 
         self.fields['people'].label = "인원"
+        self.fields['people'].widget.attrs = {'class': "form-control", 'placeholder': ""}
